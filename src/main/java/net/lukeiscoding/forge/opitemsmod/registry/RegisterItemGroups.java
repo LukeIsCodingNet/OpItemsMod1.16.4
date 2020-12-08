3b5af36cb60bf8a0622120bd68e57458bf342d0a
LukeIsCodingNet <lukeiscoding@gmail.com>
2020-12-07 19:26:41 -0500

    Renamed class to RegisterItems.java

--- src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItemGroups.java
+++ src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItemGroups.java
@@ -1,6 +1,5 @@
 package net.lukeiscoding.forge.opitemsmod.registry;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
@@ -9,35 +8,35 @@ public class RegisterItemGroups {
     public static final ItemGroup OP_ITEMS_TAB = new ItemGroup("op_items_tab") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.TEST_ITEM.get());
+            return new ItemStack(RegisterItems.TEST_ITEM.get());
         }
     };
 
     public static final ItemGroup OP_SWORDS_TAB = new ItemGroup("op_swords_tab") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.DRAGON_SLAYER.get());
+            return new ItemStack(RegisterItems.DRAGON_SLAYER.get());
         }
     };
 
     public static final ItemGroup OP_MATERIALS_TAB = new ItemGroup("op_materials_tab") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.EMERALD_NUGGET.get());
+            return new ItemStack(RegisterItems.EMERALD_NUGGET.get());
         }
     };
 
     public static final ItemGroup OP_AXES_TAB = new ItemGroup("op_axes_tab") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.EMERALD_AXE.get());
+            return new ItemStack(RegisterItems.EMERALD_AXE.get());
         }
     };
 
     public static final ItemGroup OP_PICKAXES_TAB = new ItemGroup("op_pickaxes_tab") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.EMERALD_PICKAXE.get());
+            return new ItemStack(RegisterItems.EMERALD_PICKAXE.get());
         }
     };
 }
