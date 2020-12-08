c67d34ca9a33c8ecc2ac0a67d4f95c6eb56beee5
LukeIsCodingNet <lukeiscoding@gmail.com>
2020-12-07 19:26:17 -0500

    Renamed class to RegisterItems.java

--- src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItems.java
+++ src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItems.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class RegistryHandler {
+public class RegisterItems {
 
     // create a deferred register to register items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, OpItemsMod.MOD_ID);
