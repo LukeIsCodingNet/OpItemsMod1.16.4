0b81fe500b5767521392629ac68e8ee63d039052
LukeIsCodingNet <lukeiscoding@gmail.com>
2020-12-12 20:40:26 -0500

    Added class to register blocks

---
+++ src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterBlocks.java
@@ -0,0 +1,23 @@
+package net.lukeiscoding.forge.opitemsmod.registry;
+
+import net.lukeiscoding.forge.opitemsmod.OpItemsMod;
+import net.lukeiscoding.forge.opitemsmod.blocks.RubyBlock;
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegisterBlocks {
+
+    // create a deferred register for blocks
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, OpItemsMod.MOD_ID);
+
+    // create a method to register blocks in the main class
+    public static void registerBlocks() {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // blocks...
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+}
