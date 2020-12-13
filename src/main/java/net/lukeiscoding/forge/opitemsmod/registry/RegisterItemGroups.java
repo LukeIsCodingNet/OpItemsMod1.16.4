0d1dc87abbf03f80aad95a867195ace7a6120089
LukeIsCodingNet <lukeiscoding@gmail.com>
2020-12-12 20:40:52 -0500

    Added new item groups

--- src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItemGroups.java
+++ src/main/java/net/lukeiscoding/forge/opitemsmod/registry/RegisterItemGroups.java
@@ -1,5 +1,6 @@
 package net.lukeiscoding.forge.opitemsmod.registry;
 
+import net.lukeiscoding.forge.opitemsmod.blocks.blockitems.RegisterBlockItems;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
@@ -37,6 +38,13 @@ public class RegisterItemGroups {
         @Override
         public ItemStack createIcon() {
             return new ItemStack(RegisterItems.EMERALD_PICKAXE.get());
+        }
+    };
+
+    public static final ItemGroup OP_BLOCKS_TAB = new ItemGroup("op_blocks_tab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegisterBlockItems.RUBY_BLOCK_ITEM.get());
         }
     };
 }
